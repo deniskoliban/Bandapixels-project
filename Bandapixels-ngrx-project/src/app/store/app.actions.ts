3dfe709dde9ce15d9af922a692c0919eaffc6d44
denis.koliban <deniskoliban@gmail.com>
2020-09-20 20:50:18 +0300

    создание действий

--- Bandapixels-ngrx-project/src/app/store/app.actions.ts
+++ Bandapixels-ngrx-project/src/app/store/app.actions.ts
@@ -1,4 +1,28 @@
+import { Action } from '@ngrx/store';
 
 export const CHANGE = 'CHANGE';
 export const INSCREASE = 'INSCREASE';
 export const DECREASE = 'DECREASE';
+
+export class Change implements Action {
+    readonly type = CHANGE;
+    constructor(public payload: number) {
+
+    }
+}
+
+export class Inscrease implements Action {
+    readonly type = INSCREASE;
+    constructor(public payload: number) {
+
+    }
+}
+
+export class Decrease implements Action {
+    readonly type = DECREASE;
+    constructor(public payload: number) {
+
+    }
+}
+
+
