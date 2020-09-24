6fb3fd3711eba5c73da860291fa9bff80b78e15c
denis.koliban <deniskoliban@gmail.com>
2020-09-24 20:05:08 +0300

    Исправлен функионал редюсера

--- Bandapixels-ngrx-project/src/app/store/app.reducer.ts
+++ Bandapixels-ngrx-project/src/app/store/app.reducer.ts
@@ -1,28 +1,29 @@
 
 import * as AppAction from './app.actions';
 
+
 const initialState = {
     firstNum: -5,
     secondNum: 10
 };
 
-export function usersDataReducer(state = initialState, action: AppAction.UserActionsType) {
+export function nubersReducer(state = initialState, action: AppAction.UserActionsType) {
     switch (action.type) {
-        case AppAction.CHANGE:
-            return state;
+        //  case AppAction.CHANGE:
+         //   return state;
         case AppAction.INSCREASE:
             const editStateIncrease = {...state};
             editStateIncrease.firstNum++;
             return {
                 ...state,
-                editStateIncrease
+                ...editStateIncrease
             };
         case AppAction.DECREASE:
             const editStateDecrease = {...state};
             editStateDecrease.secondNum--;
             return {
                 ...state,
-                editStateDecrease
+                ...editStateDecrease
             };
         default:
             return state;
